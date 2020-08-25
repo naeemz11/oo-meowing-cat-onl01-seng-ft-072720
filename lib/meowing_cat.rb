c0c8709bbe34b337b3669d61bcd6fcc562642a7d
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-25 18:30:59 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,19 +1,26 @@
+# class Cat 
+#   attr_accessor :name 
+  
+#   def meow
+#     puts "meow!"
+#   end 
+  
+# end
+
+
+  
+# def Cat 
+#   maru= Cat.new 
+#   maru.name= "Maru"
+#   maru.meow="meow!"
+#   puts maru.meow
+# end 
+
 class Cat 
+  
   attr_accessor :name 
   
-  def meow
-    puts "meow!"
-  end 
+  def 
   
-end
-
-
   
-def Cat 
-  maru= Cat.new 
-  maru.name= "Maru"
-  maru.meow="meow!"
-  puts maru.meow
 end 
-
-
